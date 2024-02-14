ddf2ebc40bcb72a4453702798e9256a85dd7766d
Ruben Laso <laso@par.tuwien.ac.at>
2024-02-14 14:23:38 +0100

    test: add knn test for chs::Dense2D and chs::Dense3D

--- test/source/chs_dense3d.cpp
+++ test/source/chs_dense3d.cpp
@@ -108,6 +108,62 @@ TEST(chs, chs_dense3d_spherical_search_filter)
 	}
 }
 
+TEST(chs, chs_dense_knn)
+{
+	static constexpr std::size_t num_builds = 20;
+	static constexpr std::size_t num_points = 10'000;
+	static constexpr std::size_t num_checks = 20;
+
+	static constexpr std::size_t max_k = 2'000;
+
+	for ([[maybe_unused]] const auto _ : ranges::views::indices(num_builds))
+	{
+		const auto min_coord = get_random(chs::mockup::DEFAULT_MIN_COORD - chs::mockup::DEFAULT_MAP_VARIANCE,
+		                                  chs::mockup::DEFAULT_MIN_COORD + chs::mockup::DEFAULT_MAP_VARIANCE);
+		const auto max_coord = get_random(chs::mockup::DEFAULT_MAX_COORD - chs::mockup::DEFAULT_MAP_VARIANCE,
+		                                  chs::mockup::DEFAULT_MAX_COORD + chs::mockup::DEFAULT_MAP_VARIANCE);
+
+		auto points = chs::mockup::random_points(num_points, { min_coord, min_coord, min_coord },
+		                                         { max_coord, max_coord, max_coord });
+
+		auto map = chs::Dense3D<chs::Point>(points, CELL_SIZE);
+
+		for ([[maybe_unused]] const auto i : ranges::views::indices(num_checks))
+		{
+			const auto center = chs::mockup::random_point({ min_coord, min_coord, min_coord },
+			                                              { max_coord, max_coord, max_coord });
+
+			const auto distance = [](const auto & p, const auto & q) { return arma::norm(p - q); };
+
+			const auto k = get_random({ 1 }, max_k);
+
+			const auto results_map = map.knn(k, center);
+
+			auto get_truth = [&]() {
+				using dist_point = std::pair<double, chs::Point *>;
+
+				std::vector<dist_point> dist_points;
+				dist_points.reserve(points.size());
+
+				for (auto & p : points)
+				{
+					dist_points.emplace_back(distance(p, center), &p);
+				}
+
+				ranges::actions::sort(dist_points,
+				                      [](const auto & a, const auto & b) { return a.first < b.first; });
+
+				return dist_points | ranges::views::take(k) | ranges::to<std::vector>();
+			};
+			const auto results_truth = get_truth();
+
+			const auto same = are_the_same(results_map | ranges::views::values,
+			                               results_truth | ranges::views::values);
+			EXPECT_TRUE(same);
+		}
+	}
+}
+
 
 auto main() -> int
 {
